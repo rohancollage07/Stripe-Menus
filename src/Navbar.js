af1cdeefdbe8b8bba36146eec85bc429e145d369
Rohan Dongre <rohancollege07@gmail.com>
2023-07-02 11:56:25 +0530

    ALL DONE

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,21 @@ import { useGlobalContext } from './context'
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2
+    const bottom = tempBtn.bottom - 3
 
+    openSubmenu(page, { center, bottom })
+  }
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu()
+    }
+  }
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
@@ -16,21 +28,23 @@ const Navbar = () => {
           </button>
         </div>
         <ul className="nav-links">
-           <li>
-            <button className="link-btn">products</button>
-
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
-            
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
-        <button className='btn signin-btn'>
-          Sign in
-        </button>
+        <button className="btn signin-btn">Sign in</button>
       </div>
     </nav>
   )
